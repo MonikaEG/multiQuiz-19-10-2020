283effed595a50f2c95da89d0cd83455ea5c930f
Monika <monikaegeberg@gmail.com>
2020-10-18 19:44:20 +0200

    first commit

---
+++ Forsøg med modules/multiQuizImport-m.js
@@ -0,0 +1,1 @@
+import {questions, score} from './multiQuiz.js'
---
+++ multiQuiz.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//List of array objects (questions in this case)
+var questions = [
+    {
+        prompt: "In which Danish city is IBA located?\n(a) Aalborg\n\(b) Esbjerg\n(c) Haderslev\n(d) Kolding",
+        answer: "d"
+},
+{
+        prompt: "Which education programme does IBA not offer?\n(a) Multimediadesign\n\(b) Financial controller\n(c) Architect\n(d) Web development",
+        answer: "c"
+},
+{
+        prompt: "What color is Dannebrog, Denmark's flag?\n(a) Red/white\n\(b) Purple/yellow\n(c) Blue/white\n(d) Green/blue",
+        answer: "a"
+},
+] 
+
+var score = 0;
+
+for(var i=0; i < questions.length; i++){ //Loops for as many questions there are in the questions array.
+    var response = window.prompt(questions[i].prompt);
+        if(response == questions[i].answer){ //If the response to the current question is the right answer...
+        score++;
+        alert("Correct!");
+     } else {
+        alert("Wrong!"); //If the response to the current question is wrong...
+        }
+}
+
+alert("You got " + score + "/" + questions.length);
+
