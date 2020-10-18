dac4c807e748cfa370df72c0687993d3e009d063
Monika <monikaegeberg@gmail.com>
2020-10-18 21:35:38 +0200

    second commit

--- Forsøg med modules/multiQuiz-m.js
+++ Forsøg med modules/multiQuiz-m.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //List of array objects (questions in this case)
-export var questions = [
+var questions = [
     {
         prompt: "In which Danish city is IBA located?\n(a) Aalborg\n\(b) Esbjerg\n(c) Haderslev\n(d) Kolding",
         answer: "d"
@@ -14,19 +14,10 @@ export var questions = [
         prompt: "What color is Dannebrog, Denmark's flag?\n(a) Red/white\n\(b) Purple/yellow\n(c) Blue/white\n(d) Green/blue",
         answer: "a"
 },
-] 
+]
 
-export var score = 0;
+var score = 0;
 
-for(var i=0; i < questions.length; i++){ //Loops for as many questions there are in the questions array.
-    var response = window.prompt(questions[i].prompt);
-        if(response == questions[i].answer){ //If the response to the current question is the right answer...
-        score++;
-        alert("Correct!");
-     } else {
-        alert("Wrong!"); //If the response to the current question is wrong...
-        }
-}
+import {scoreCheck} from './multiQuizImport-m.js';
 
 alert("You got " + score + "/" + questions.length);
-
--- Forsøg med modules/multiQuizImport-m.js
+++ Forsøg med modules/multiQuizImport-m.js
@@ -1,1 +1,13 @@
-import {questions, score} from './multiQuiz.js'
+'use strict';
+
+export function scoreCheck { //Gives the entire loop and if sentence section a name in order to export it?
+    for(var i=0; i < questions.length; i++){ //Loops for as many questions there are in the questions array.
+    var response = window.prompt(questions[i].prompt);
+        if(response == questions[i].answer){ //If the response to the current question is the right answer...
+        score++;
+        alert("Correct!");
+     } else {
+        alert("Wrong!"); //If the response to the current question is wrong...
+        }
+    }
+}
